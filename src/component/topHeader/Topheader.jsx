5a4949a435255f0b40add5fa88fbf46c47dce5bb
vikramwatkar96 <vikramwatkar96@gmail.com>
2024-07-30 12:22:45 +0530

    Css Changed for top header

--- src/component/topHeader/Topheader.jsx
+++ src/component/topHeader/Topheader.jsx
@@ -1,34 +1,41 @@
 import React from 'react'
-import '../../App.css'
+import '../../css_style/topHeader.css'
 
 function Topheader() {
     return (
         <header>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div>
-                            <ul className="header-list">
-                                <li>Welcome to Our store Multikart</li>
-                                <li><span><i className="bi bi-telephone-fill"></i></span> Call Us: +919834707617</li>
-                            </ul>
+            <div className="top-header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="header-contact">
+                                <ul>
+                                    <li>Welcome to Our store Multikart</li>
+                                    <li><i className="fa fa-phone text-white" aria-hidden="true"></i>Call Us: 123 - 456 -
+                                        7890
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="left">
-                            <ul className="header-list">
-                                <a href="">
-                                    <li><span><i className="bi bi-heart-fill"></i> Wishlist</span></li>
-                                </a>
-                                <a href="">
-                                    <li><span><i className="bi bi-person-fill"></i></span> Login</li>
-                                </a>
+                        <div className="text-end col-lg-6">
+                            <ul className="header-dropdown">
+                                <li className="mobile-wishlist"><a href="/page/account/wishlist"><i className="fa fa-heart"
+                                    aria-hidden="true"></i> wishlist</a></li>
+                                <li className="onhover-dropdown mobile-account"><i className="fa fa-user"
+                                    aria-hidden="true"></i> My
+                                    Account<ul className="onhover-show-div">
+                                        <li><a href="/page/account/login">Login</a></li>
+                                        <li><a href="/page/account/register">Register</a></li>
+                                        <li><a>Logout</a></li>
+                                    </ul>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </header>
+
     )
 }
 
